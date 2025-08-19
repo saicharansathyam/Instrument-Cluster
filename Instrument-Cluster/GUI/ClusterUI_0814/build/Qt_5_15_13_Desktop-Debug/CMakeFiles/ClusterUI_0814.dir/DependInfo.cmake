555ece7cb430e1db1ca1fad4f601657d2455a094
Kijin Baek <429jin@kookmin.ac.kr>
2025-08-19 14:50:29 +0200

    GUI
    Updated

---
+++ Instrument-Cluster/GUI/ClusterUI_0814/build/Qt_5_15_13_Desktop-Debug/CMakeFiles/ClusterUI_0814.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "ClusterUI_0814_autogen/timestamp" "custom" "ClusterUI_0814_autogen/deps"
+  "/home/kijin-baek/Desktop/Instrument-Cluster/Instrument-Cluster/GUI/ClusterUI_0814/build/Qt_5_15_13_Desktop-Debug/ClusterUI_0814_autogen/EWIEGA46WW/qrc_qml.cpp" "CMakeFiles/ClusterUI_0814.dir/ClusterUI_0814_autogen/EWIEGA46WW/qrc_qml.cpp.o" "gcc" "CMakeFiles/ClusterUI_0814.dir/ClusterUI_0814_autogen/EWIEGA46WW/qrc_qml.cpp.o.d"
+  "/home/kijin-baek/Desktop/Instrument-Cluster/Instrument-Cluster/GUI/ClusterUI_0814/build/Qt_5_15_13_Desktop-Debug/ClusterUI_0814_autogen/mocs_compilation.cpp" "CMakeFiles/ClusterUI_0814.dir/ClusterUI_0814_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/ClusterUI_0814.dir/ClusterUI_0814_autogen/mocs_compilation.cpp.o.d"
+  "/home/kijin-baek/Desktop/Instrument-Cluster/Instrument-Cluster/GUI/ClusterUI_0814/PiRacerBridge.cpp" "CMakeFiles/ClusterUI_0814.dir/PiRacerBridge.cpp.o" "gcc" "CMakeFiles/ClusterUI_0814.dir/PiRacerBridge.cpp.o.d"
+  "/home/kijin-baek/Desktop/Instrument-Cluster/Instrument-Cluster/GUI/ClusterUI_0814/main.cpp" "CMakeFiles/ClusterUI_0814.dir/main.cpp.o" "gcc" "CMakeFiles/ClusterUI_0814.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Instrument-Cluster/GUI/ClusterUI_0814/build/Qt_5_15_13_Desktop-Debug/CMakeFiles/ClusterUI_0814_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClusterUI_0814_autogen"
+  "ClusterUI_0814_autogen/mocs_compilation.cpp"
+  "ClusterUI_0814_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ClusterUI_0814_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
